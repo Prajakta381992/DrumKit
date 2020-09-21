f23c8f3f8bf8b266456e1fe6b0401adb0bb65820
Prajakta381992 <71488327+Prajakta381992@users.noreply.github.com>
2020-09-21 11:27:12 +0530

    Update index.js
    New features added

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i<numberOfDrumButtons; i++) {
-document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+
+});
 }
 
-function handleClick() {
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key)
+});
+
+function makeSound(key) {
 
   //console.log(this);
   //this.style.color = "white";
-
-  var buttonInnerHTML = this.innerHTML;
-  switch (buttonInnerHTML) {
+  switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -40,7 +49,17 @@ function handleClick() {
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
     break;
-    default:
+    default: console.log(key);
 
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  },100);
+
 }
